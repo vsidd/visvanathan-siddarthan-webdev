608cbfe15bda45adb01134051a77c2cc20351bfb
vsidd <siddarth@ccs.neu.edu>
2016-09-18 10:23:22 -0400

    adding server.js changes for heroku

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var ipaddress = process.env.IP;
+var port      = process.env.PORT || 3000;
 
 app.listen(port, ipaddress);
