00db68a2caf6eb2d5b1f02cfe452fd0776b5aa4b
vsidd <siddarth@ccs.neu.edu>
2016-10-01 21:24:43 -0400

    Changing mongodb connection string

--- test/app.js
+++ test/app.js
@@ -4,8 +4,8 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    // var connectionString = 'mongodb://heroku_2kf5s1tp:d90a6e4kc0cmta7ei7a3mlftse@ds033056.mlab.com:33056/heroku_2kf5s1tp';
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'mongodb://heroku_2kf5s1tp:d90a6e4kc0cmta7ei7a3mlftse@ds033056.mlab.com:33056/heroku_2kf5s1tp';
+    // var connectionString = 'mongodb://127.0.0.1:27017/test';
     if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
         connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
             process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
