7f687f3ddd2945b6bcfac85f35f12f65bdf15891
vsidd <siddarth@ccs.neu.edu>
2016-12-13 23:30:15 -0500

    mongo change

--- project/model/model.server.js
+++ project/model/model.server.js
@@ -3,8 +3,8 @@
  */
 
 module.exports = function () {
-    // var connectionString = 'mongodb://heroku_2kf5s1tp:d90a6e4kc0cmta7ei7a3mlftse@ds033056.mlab.com:33056/heroku_2kf5s1tp';
-    var connectionString = 'mongodb://127.0.0.1:27017/webdev-project-db';
+    var connectionString = 'mongodb://heroku_2kf5s1tp:d90a6e4kc0cmta7ei7a3mlftse@ds033056.mlab.com:33056/heroku_2kf5s1tp';
+    // var connectionString = 'mongodb://127.0.0.1:27017/webdev-project-db';
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
